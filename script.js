7fe1598c5312e533921a392d90c9ff90fd43a401
MangEs23 <tempestdragon987@gmail.com>
2023-05-08 01:01:02 +0700

    add app calculator sederhana

---
+++ script.js
@@ -0,0 +1,28 @@
+const calc = (num1, num2, operator) => {
+  let hasil =
+    operator == "+"
+      ? num1 + num2
+      : operator == "-"
+      ? num1 - num2
+      : operator == "/"
+      ? num1 / num2
+      : operator == "*"
+      ? num1 * num2
+      : null;
+  return hasil;
+};
+
+function runner(button) {
+  var num1 = parseFloat(document.calculator.num1.value);
+  var num2 = parseFloat(document.calculator.num2.value);
+  var operator = button.value;
+  var hasil = calc(num1, num2, operator);
+  document.calculator.hasil.value = hasil;
+}
+
+let clickedValue;
+
+function handleClick(button) {
+  clickedValue = button.value;
+  console.log(clickedValue);
+}
